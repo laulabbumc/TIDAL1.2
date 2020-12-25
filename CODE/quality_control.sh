cb23e0cf66b0567db5cb16a0258d676057c90a07
Reazur Rahman <reazur@scc1.bu.edu>
2020-12-25 00:51:20 -0500

    updated documentation and removed deprecated files

--- CODE/quality_control.sh
+++
@@ -1,9 +0,0 @@
-input=$1
-output=$2
-avgquality="20"
-
-#with log file
-#java -jar ~reazur/SOFTWARE/Trimmomatic-0.30/trimmomatic-0.30.jar SE -phred33 -trimlog log.txt $input $output CROP:45 HEADCROP:6 LEADING:25 TRAILING:25 AVGQUAL:$avgquality MINLEN:85
-
-#without log file
-java -jar ~reazur/SOFTWARE/Trimmomatic-0.30/trimmomatic-0.30.jar SE -phred33 $input $output LEADING:20 TRAILING:20 AVGQUAL:$avgquality
